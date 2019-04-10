151d8fad288ccd55cd84e27c237a0c52a89a74bc
ills5771 <ilyayelizarov1@gmail.com>
2019-04-10 13:01:23 -0700

    adds snapshot test for Dashboard and test to check if gae is locked when pressing lock button

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -6,4 +6,19 @@ import "react-testing-library/cleanup-after-each";
 
 import Dashboard from "./Dashboard.js";
 
-describe("<Dashboard />");
+describe("<Dashboard />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("<Controls />", () => {
+  it("should lock gate when pressing lock gate button", () => {
+    const { getByText } = render(<Dashboard />);
+    const lockButton = getByText(/lock gate/i);
+    fireEvent.click(lockButton);
+
+    getByText(/locked/i);
+  });
+});
