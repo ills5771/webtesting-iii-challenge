2a7aec5b773521c5ce47ffd8c69a33be1093332b
ills5771 <ilyayelizarov1@gmail.com>
2019-04-10 12:40:40 -0700

    starts adding libraries to Dashboard.spec.js

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
 // Test away
+import { render, fireEvent, cleanup } from "react-testing-library";
+import React from "react";
+import renderer from "react-test-renderer";
+import "react-testing-library/cleanup-after-each";
+
+import Dashboard from "./Dashboard.js";
+
+describe("<Dashboard />");
